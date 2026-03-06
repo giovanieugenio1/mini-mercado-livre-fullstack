7f1d1cf5e64dafafad4881d4fedda8fd2e0eaf8d
Giovani Eugenio Jesuino <giovani.eugenio.sp@hotmail.com>
2026-03-06 09:56:52 -0300

    modificado pom.xml para add user-service ao modules | criado gitignore

--- services/payment-service/target/classes/db/migration/V2__add_items_json.sql
+++
@@ -1,3 +0,0 @@
--- Adiciona items_json para repassar a lista de produtos ao inventory-service
--- via payment.authorized.v1 (saga coreografada sem necessidade de chamada HTTP)
-ALTER TABLE payment ADD COLUMN items_json TEXT;
